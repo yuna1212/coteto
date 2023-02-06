6116a114ff7915c1b1af119d487924b9a07f2066
yuna1212 <yunachong26@gmail.com>
2023-02-06 19:40:13 +0900

    fix: bsd 명령어로 수정

--- fin.sh
+++ fin.sh
@@ -21,18 +21,19 @@ echo "완료 시간: $now_time"
 
 # 파일에서 시작 시간 읽어오기
 start_time_line=$(sed -n 2p "$file_path")
-start_time=$(echo $start_time_line | grep -oP '(19|20)\d{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01]) (0[1-9]|1[012]):[0-5][0-9] [AP]M')
+date_pattern='(19|20)\d{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01]) (0[1-9]|1[012]):[0-5][0-9] [AP]M'
+start_time=$(echo $start_time_line | perl -nle "print $& while m{$date_pattern}g")
 echo $start_time_line
 
 # 시작 시간에서 현재 시간 빼기
-now_sec=$(date -d "$now_time" +%s)
-start_sec=$(date -d "$start_time" +%s)
+now_sec=$(date -j -f "%F %I:%M %p"  "$now_time" +%s)
+start_sec=$(date -j -f "%F %I:%M %p"  "$start_time" +%s)
 
 sec_diff=$(($now_sec-$start_sec))
 min_diff=$(($sec_diff/60))
 hour_diff=$(($min_diff/60))
 min_diff=$(($min_diff%60))
-echo "소요 시간: $hour_diff시간 $min_diff분"
+echo 소요 시간: $hour_diff'시간' $min_diff'분'
 
 # 10분 단위로 반올림하기
 min_apprx=$((($min_diff+5)/10*10))
@@ -42,15 +43,15 @@ min_apprx=$(($min_apprx%60))
 consumed_time_format="-분"
 if [ $hour_apprx -eq 0 ]
 then
-    consumed_time_format="$min_apprx분"
+    consumed_time_format=$min_apprx'분'
 elif [ $min_apprx -eq 0 ]
 then
-    consumed_time_format="$hour_apprx시간"
+    consumed_time_format=$hour_apprx'시간'
 else
-    consumed_time_format="$hour_apprx시간 $min_apprx분"
+    consumed_time_format=$hour_apprx'시간 '$min_apprx'분'
 fi
 
-echo -e "기록한 시간: \e[1;93m$consumed_time_format\e[0m" 
+echo "기록한 시간: $consumed_time_format" 
 
 # 파일에 소요 시간 기록
-sed -i "3s/.*/소요 시간: $consumed_time_format/" "$file_path"
+sed -i "" "3s/.*/소요 시간: $consumed_time_format/" "$file_path"
